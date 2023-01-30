90bf5dc62d6800cd1d0eefb6b80d61897792fd89
ivenckunas <ignas.venckunas@gmail.com>
2023-01-30 17:41:28 +0200

    login inputs, refs

--- reactjs/src/components/LoginReg/Login.js
+++ reactjs/src/components/LoginReg/Login.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
 import "./login.css";
 
 function Login() {
+	const nickRef = useRef();
+	const passRef = useRef();
+
+	const handleLogin = () => {
+		const loginObj = {
+			nick: nickRef.current.value,
+			pass: passRef.current.value,
+		};
+
+		console.log("loginObj ===", loginObj);
+	};
+
 	return (
 		<div className="login">
 			<h2>Login</h2>
+			<div className="login-inputs">
+				<input
+					ref={nickRef}
+					type="text"
+					placeholder="nickname"
+				/>
+				<input
+					ref={passRef}
+					type="password"
+					placeholder="password"
+				/>
+			</div>
+			<button
+				className="login-btn"
+				onClick={handleLogin}
+			>
+				login
+			</button>
 		</div>
 	);
 }
