ad79c9011ce3ebba4e03bad232176fc9c14905ec
ivenckunas <ignas.venckunas@gmail.com>
2023-01-30 17:25:16 +0200

    login, reg page

---
+++ reactjs/src/pages/LoginRegPage/LoginRegPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../../components/LoginReg/Login";
+import Register from "../../components/LoginReg/Register";
+
+function LoginReg() {
+	return (
+		<div>
+			<Login />
+			<Register />
+		</div>
+	);
+}
+
+export default LoginReg;
