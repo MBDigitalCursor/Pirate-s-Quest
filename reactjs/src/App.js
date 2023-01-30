28816be45fb3144b8728abdbacaf0e7abb0ec19d
Viktoras <pastodezute14@gmail.com>
2023-01-30 16:04:49 +0200

    mano pakeitimas

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -12,9 +12,16 @@ function App() {
 		});
 	};
 
+	const test2 = () => {
+		axios.post("http://localhost:5000/test", obj).then(function (response) {
+			console.log(response);
+		});
+	};
+
 	return (
 		<div className='App'>
 			<button onClick={test}>test</button>
+			<button onClick={test2}>test2</button>
 			<h2>Test</h2>
 		</div>
 	);
