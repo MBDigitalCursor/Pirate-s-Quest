e730d8a60c1c176d210edb757a52f5f73d70ba81
Viktoras <pastodezute14@gmail.com>
2023-01-30 16:07:33 +0200

    istryniau nx viska

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,29 +2,10 @@ import axios from "axios";
 import "../src/css/App.css";
 
 function App() {
-	const obj = {
-		mess: "test",
-	};
-
-	const test = () => {
-		axios.post("http://localhost:5000/test", obj).then(function (response) {
-			console.log(response);
-		});
-	};
-
-	const test2 = () => {
-		axios.post("http://localhost:5000/test", obj).then(function (response) {
-			console.log(response);
-		});
-	};
 
 	return (
 		<div className="App">
-			<button onClick={test}>test</button>
-			<button onClick={test2}>test2</button>
-			<h2>Test</h2>
-			<h2>Test VK</h2>
-			<h3>Test</h3>
+
 		</div>
 	);
 }
