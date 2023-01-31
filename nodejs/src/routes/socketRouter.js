5408cafcc003750aaea951c51b99e4114ab0cd54
Arturas <mixmln14@gmail.com>
2023-01-31 20:22:31 +0200

    exp

--- nodejs/src/routes/socketRouter.js
+++ nodejs/src/routes/socketRouter.js
@@ -11,10 +11,14 @@ module.exports = (io) => {
 						$inc: {
 							gold: 1 + foundUser.upgrades.dropPerClickLevel / 10,
 						},
+						$inc: {
+							rank: {
+								exp: 1,
+							},
+						},
 					}
 				);
 				const updatedUser = await UserSchema.findOne({ id });
-				console.log("updatedUser ===", updatedUser);
 				socket.emit("updatedUser", updatedUser);
 			}
 		});
