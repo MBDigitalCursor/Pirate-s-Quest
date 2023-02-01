6cb2ccfbc9bd12b806309f4afd6adf3163376833
ivenckunas <ignas.venckunas@gmail.com>
2023-02-01 15:17:42 +0200

    upgrades card

---
+++ reactjs/src/components/UpgradesComp/UpgradeCard.js
@@ -0,0 +1,112 @@
+import { Button, Popper, Stack } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import MainContext from "../../context/MainContext";
+import HelpIcon from "@mui/icons-material/Help";
+import upgradeData from "../../helpers/upgrades.json";
+import "./upgradeCard.css";
+
+function UpgradeCard({ singleUpg, idx }) {
+	const { dispatch, socket } = useContext(MainContext);
+	console.log("singleUpg ===", singleUpg);
+	const { logged } = useSelector((state) => state.appStore);
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+
+	const handleClick = (event) => {
+		setAnchorEl(anchorEl ? null : event.currentTarget);
+	};
+
+	const upgrade = (upgrade) => {
+		const data = {
+			userId: logged.id,
+			upgrade,
+		};
+		socket.emit("upgrade", data);
+	};
+
+	const open = Boolean(anchorEl);
+	const id = open ? "simple-popper" : undefined;
+
+	return (
+		<Box
+			sx={{
+				maxHeight: "35px",
+				border: "1px solid slategrey",
+				p: "0.3rem",
+				m: "0.2rem",
+			}}
+		>
+			<Stack
+				direction="row"
+				sx={{
+					justifyContent: "space-between",
+					alignItems: "center",
+				}}
+			>
+				<Stack
+					direction="row"
+					sx={{
+						alignItems: "center",
+					}}
+				>
+					<img
+						className="upgrade-logo"
+						src={upgradeData[idx].img}
+						alt="upgrade logo"
+					/>
+					<Box className="upgrade-info">
+						<h5>
+							{upgradeData[idx].title} <span>${singleUpg.upgradeCost.toFixed(0)}</span>
+						</h5>
+					</Box>
+				</Stack>
+				<Box>
+					<Button
+						color="gold"
+						variant="contained"
+						sx={{
+							fontSize: "0.8rem",
+							fontWeight: "bold",
+							padding: "0.2rem",
+							marginRight: "0.3rem",
+						}}
+					>
+						upgrade
+					</Button>
+				</Box>
+				<Box>
+					<Box
+						onMouseEnter={handleClick}
+						onMouseLeave={() => setAnchorEl(false)}
+						sx={{
+							width: "max-content",
+							transition: "0.2s",
+						}}
+					>
+						<HelpIcon
+							sx={{
+								fontSize: "1.2rem",
+								transition: "0.3s",
+								"&:hover": {
+									border: "none",
+									color: "#F6AA1C",
+								},
+							}}
+						/>
+					</Box>
+					<Popper
+						id={id}
+						open={open}
+						anchorEl={anchorEl}
+					>
+						<Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>The content of the Popper.</Box>
+					</Popper>
+				</Box>
+			</Stack>
+		</Box>
+	);
+}
+
+export default UpgradeCard;
--- reactjs/src/components/UpgradesComp/UpgradesWindow.js
+++ reactjs/src/components/UpgradesComp/UpgradesWindow.js
@@ -1,36 +1,21 @@
-import { Button, Popover, Popper, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useContext } from "react";
 import { useSelector } from "react-redux";
 import MainContext from "../../context/MainContext";
+import UpgradeCard from "./UpgradeCard";
 
 function UpgradesWindow() {
 	const { dispatch, socket } = useContext(MainContext);
 
 	const { logged } = useSelector((state) => state.appStore);
 
-	const [anchorEl, setAnchorEl] = React.useState(null);
-
-	const handleClick = (event) => {
-		setAnchorEl(anchorEl ? null : event.currentTarget);
-	};
-
-	const upgrade = (upgrade) => {
-		const data = {
-			userId: logged.id,
-			upgrade,
-		};
-		socket.emit("upgrade", data);
-	};
-
-	const open = Boolean(anchorEl);
-	const id = open ? "simple-popper" : undefined;
+	const upgradesArray = logged.upgrades;
 
 	return (
 		<div
 			style={{
-				height: "600px",
-				padding: "10px 5px",
+				height: "610px",
+				padding: "5px 5px",
 				boxSizing: "border-box",
 				boxShadow: "3px 3px 10px 1px #3b3939ad",
 				borderRadius: "4px",
@@ -38,25 +23,23 @@ function UpgradesWindow() {
 				backdropFilter: "blur(4px)",
 			}}
 		>
-			<div>
-				<Button
-					aria-describedby={id}
-					type="button"
-					onMouseEnter={handleClick}
-					onMouseLeave={() => setAnchorEl(false)}
-				>
-					Toggle Popper
-				</Button>
-				<Popper
-					id={id}
-					open={open}
-					anchorEl={anchorEl}
-				>
-					<Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>The content of the Popper.</Box>
-				</Popper>
-			</div>
+			<h1>Upgrades</h1>
 
-			<h1 onClick={() => upgrade("dropPerClickLevel")}>Upgrades</h1>
+			<Box
+				sx={{
+					height: "50px",
+					display: "flex",
+					flexDirection: "column",
+				}}
+			>
+				{upgradesArray.map((singleUpg, i) => (
+					<UpgradeCard
+						idx={i}
+						key={i}
+						singleUpg={singleUpg}
+					/>
+				))}
+			</Box>
 		</div>
 	);
 }
