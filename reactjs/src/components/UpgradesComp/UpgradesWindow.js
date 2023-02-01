0f80f47388334e849483ee4707f7dca17cb89515
ivenckunas <ignas.venckunas@gmail.com>
2023-02-01 14:20:21 +0200

    working popper

--- reactjs/src/components/UpgradesComp/UpgradesWindow.js
+++ reactjs/src/components/UpgradesComp/UpgradesWindow.js
@@ -13,52 +13,52 @@ function UpgradesWindow() {
 
 	const handleClick = (event) => {
 		setAnchorEl(anchorEl ? null : event.currentTarget);
-
-		const upgrade = (upgrade) => {
-			const data = {
-				userId: logged.id,
-				upgrade,
-			};
-			socket.emit("upgrade", data);
-		};
-
-		const open = Boolean(anchorEl);
-		const id = open ? "simple-popper" : undefined;
-
-		return (
-			<div
-				style={{
-					height: "600px",
-					padding: "10px 5px",
-					boxSizing: "border-box",
-					boxShadow: "3px 3px 10px 1px #3b3939ad",
-					borderRadius: "4px",
-					width: "18rem",
-					backdropFilter: "blur(4px)",
-				}}
-			>
-				<div>
-					<button
-						aria-describedby={id}
-						type="button"
-						onMouseEnter={handleClick}
-						onMouseLeave={() => setAnchorEl(false)}
-					>
-						Toggle Popper
-					</button>
-					<Popper
-						id={id}
-						open={open}
-						anchorEl={anchorEl}
-					>
-						<Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>The content of the Popper.</Box>
-					</Popper>
-				</div>
-
-				<h1 onClick={() => upgrade("dropPerClickLevel")}>Upgrades</h1>
-			</div>
-		);
 	};
+
+	const upgrade = (upgrade) => {
+		const data = {
+			userId: logged.id,
+			upgrade,
+		};
+		socket.emit("upgrade", data);
+	};
+
+	const open = Boolean(anchorEl);
+	const id = open ? "simple-popper" : undefined;
+
+	return (
+		<div
+			style={{
+				height: "600px",
+				padding: "10px 5px",
+				boxSizing: "border-box",
+				boxShadow: "3px 3px 10px 1px #3b3939ad",
+				borderRadius: "4px",
+				width: "18rem",
+				backdropFilter: "blur(4px)",
+			}}
+		>
+			<div>
+				<Button
+					aria-describedby={id}
+					type="button"
+					onMouseEnter={handleClick}
+					onMouseLeave={() => setAnchorEl(false)}
+				>
+					Toggle Popper
+				</Button>
+				<Popper
+					id={id}
+					open={open}
+					anchorEl={anchorEl}
+				>
+					<Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>The content of the Popper.</Box>
+				</Popper>
+			</div>
+
+			<h1 onClick={() => upgrade("dropPerClickLevel")}>Upgrades</h1>
+		</div>
+	);
 }
 
 export default UpgradesWindow;
